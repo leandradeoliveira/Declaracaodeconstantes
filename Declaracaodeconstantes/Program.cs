bfccc7295a0a9a5090728452096c7752cd758a82
LENOVO <LENOVO@LAPTOP-5935NBF5>
2024-09-12 18:13:44 -0300

    Adicionar arquivos de projeto.

---
+++ Declaracaodeconstantes/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Declaracaodeconstantes
+{
+    public enum TerrainEnum
+    {
+        GRASS,
+        SAND,
+        WATER,
+        WALL
+    }
+
+    // Cores e caracteres de acordo com o tipo de terreno (GetColor e GetChar, respectivamente). 
+    public static class TerrainEnumExtensions
+    {
+        public static ConsoleColor GetColor(this TerrainEnum terrain)
+        {
+            switch (terrain)
+            {
+                case TerrainEnum.GRASS: return ConsoleColor.Green;
+                case TerrainEnum.SAND: return ConsoleColor.Yellow;
+                case TerrainEnum.WATER: return ConsoleColor.Blue;
+                default: return ConsoleColor.DarkGray;
+            }
+        }
+
+        public static char GetChar(this TerrainEnum terrain)
+        {
+            switch (terrain)
+            {
+                case TerrainEnum.GRASS: return '\u201c'; // “
+                case TerrainEnum.SAND: return '\u25cb'; // ○
+                case TerrainEnum.WATER: return '\u2248'; // ≈
+                default: return '\u25cf'; // ●
+            }
+        }
+    }
+
+    // Escopo do método Main na classe Program.
+    class Program
+    {
+        static TerrainEnum[,] map =
+        {
+        { TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS },
+        { TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.SAND },
+        { TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WALL, TerrainEnum.WATER, TerrainEnum.WATER },
+        { TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS },
+        { TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.SAND },
+        { TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WALL, TerrainEnum.WATER, TerrainEnum.WATER },
+        { TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS },
+        { TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.SAND },
+        { TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WALL, TerrainEnum.WATER, TerrainEnum.WATER },
+        { TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS },
+        { TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.SAND } };
+
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            for (int row = 0; row < map.GetLength(0); row++)
+            {
+                for (int column = 0; column < map.GetLength(1); column++)
+                {
+                    Console.ForegroundColor = map[row, column].GetColor();
+                    Console.Write(map[row, column].GetChar() + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
